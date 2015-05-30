3d1cb9f54a4e842e0489cccdb9a5e385e19ea00e
u-saku13 <u.saku13@gmail.com>
2015-05-30 15:15:05 +0900

    connectSQL.py

---
+++ connectSQL.py
@@ -0,0 +1,28 @@
+﻿  # -*- coding: utf-8 -*-
+import mysql.connector
+
+
+config = {
+    'user': 'root',
+    'password': '',
+    'database':'',
+    'host' : '',
+    'charset' : 'sjis'
+}
+
+cnx = mysql.connector.connect(**config)
+cur = cnx.cursor(buffered=True)
+#cur.execute("SET NAMES sjis")
+sql ="SELECT * FROM  WHERE '"
+
+cur.execute(sql)
+rows = cur.fetchall()
+for row in rows:
+    print(row[0])
+
+###DATE, VALUE
+##for (DATE, VALUE) in cur:
+##    print(DATE, VALUE)
+
+cur.close()
+cnx.close()
